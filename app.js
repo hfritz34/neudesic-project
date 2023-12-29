6c7266afa62b8e7e3b7c5de2867a458bca018fee
hfritz34 <133249998+hfritz34@users.noreply.github.com>
2023-12-28 18:16:36 -0700

    simple web server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Loan Amortization Calc');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); 
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
